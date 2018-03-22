3c9c865d588a7418db66dcf64fbc1b520ea3ac75
alexpilk <alex.pilkevych@gmail.com>
2018-03-22 10:03:57 +0100

    Memory access

--- lab3/fib/fib.s
+++ lab3/fib/fib.s
@@ -4,28 +4,38 @@ SYSWRITE = 4
 STDOUT = 1
 SUCCESS_CODE = 0
 
-N = 14
+N = 6
 
-.data
+.bss
+.comm prev, 4
+.comm curr, 4
+# previous db 0abcdh
+.equ previous, 84
 .text
 .global _start
 
 /*
-%edi - 
-%esi - buffer index register
-%edx - holds number characters before they are added to the buffer (remainder register)
-%ebx - divisor register
+%edi - previous number
+%esi - current number
 */
 
 _start:
-mov $2, %ebx
+mov $0, %eax  #memory index
 
-mov $0, %edi
-mov $1, %esi
+mov $2, %ebx  # start fib counter (0 and 1 fibonacci numbers are hardcoded)
+
+
+mov $0, %edi  # first fib number
+mov $1, %esi  # second fib number
+mov %edi, prev(,%eax,4)
+mov %esi, curr(,%eax,4)
+
 
 jmp fibonacci_generator
 
 fibonacci_generator:
+mov prev(,%eax,4), %edi  # load prev from memory
+mov curr(,%eax,4), %esi  # load prev from memory
 add %esi, %edi  # add two numbers and store result in %edi
 mov %esi, %edx  # copy %esi (old largest number) to temp register %edx
 mov %edi, %esi  # copy the new largest number (%edi) to %esi
@@ -33,6 +43,10 @@ mov %edx, %edi  # copy old largest number (%edx) to new smallest number (%edi)
 cmp $N, %ebx
 je exit
 inc %ebx
+
+mov %edi, prev(,%eax,4)  # load results back into memory
+mov %esi, curr(,%eax,4)
+
 jmp fibonacci_generator
 
 exit:
