0f7a69c7cd012710a5a748978bdbdb8287b28569
Alex <alex.pilkevych@gmail.com>
2018-04-04 20:14:07 +0300

    Added the full fibonacci code

--- lab3/fib/fib.s
+++ lab3/fib/fib.s
@@ -4,52 +4,63 @@ SYSWRITE = 4
 STDOUT = 1
 SUCCESS_CODE = 0
 
-N = 6
+# 47 correctly outputs 0xb1    0x19    0x24    0xe1
+# which is 2971215073
+# It is the largest fibonacci number that can be represented on 4 bytes
+BYTES_AVAILABLE = 4
+N = 47
 
 .bss
 .comm prev, 4
 .comm curr, 4
-# previous db 0abcdh
-.equ previous, 84
+
 .text
 .global _start
 
-/*
-%edi - previous number
-%esi - current number
-*/
 
 _start:
-mov $0, %eax  #memory index
+mov $3, %eax  # memory register
+movb $0, %dh  # first fib number
+movb $1, %dl  # second fib number
+movb %dh, prev(,%eax,1)  # move starting numbers to memory
+movb %dl, curr(,%eax,1)
+mov $1, %ecx  # start fib counter from 2 (0 and 1 fibonacci numbers are hardcoded)
+jmp count_fibonacci_numbers
 
-mov $2, %ebx  # start fib counter (0 and 1 fibonacci numbers are hardcoded)
+count_fibonacci_numbers:
+cmp $N, %ecx  # check if the given number N has been found
+je exit  # if so - exit
+inc %ecx  # else increment the counter and add prev to curr
+jmp big_endian_add
 
+big_endian_add:
+mov $BYTES_AVAILABLE, %eax  # %eax = digit position counter, starting from end
+mov $0, %ebx  # reset %ebx (%ebx = carry register)
+mov $1, %edi  # %edi = default carry value
 
-mov $0, %edi  # first fib number
-mov $1, %esi  # second fib number
-mov %edi, prev(,%eax,4)
-mov %esi, curr(,%eax,4)
+big_endian_add_loop:
+sub $1, %eax
+movb prev(,%eax,1), %dh  # copy previous number to %dh
+movb curr(,%eax,1), %dl  # copy current number to %dl
+movb %dl, prev(,%eax,1)  # overwrite previous number in memory with the current number
 
+add  %bl, %dl  # add carry do destination
+mov $0, %ebx  # reset carry
+cmovc %edi, %ebx  # set carry to default carry value if carry eflag is set
+addb  %dh, %dl  # add previous number to current
+cmovc %edi, %ebx  # set carry to default carry value if carry eflag is set
 
-jmp fibonacci_generator
+movb %dl, curr(,%eax,1)  # save results on current position to memory
 
-fibonacci_generator:
-mov prev(,%eax,4), %edi  # load prev from memory
-mov curr(,%eax,4), %esi  # load prev from memory
-add %esi, %edi  # add two numbers and store result in %edi
-mov %esi, %edx  # copy %esi (old largest number) to temp register %edx
-mov %edi, %esi  # copy the new largest number (%edi) to %esi
-mov %edx, %edi  # copy old largest number (%edx) to new smallest number (%edi)
-cmp $N, %ebx
-je exit
-inc %ebx
+cmp $0, %eax  # exit if digit position is zero
+je exit_big_endian_add
+jmp big_endian_add_loop  # otherwise continue to the next digit
 
-mov %edi, prev(,%eax,4)  # load results back into memory
-mov %esi, curr(,%eax,4)
+exit_big_endian_add:
+jmp count_fibonacci_numbers
 
-jmp fibonacci_generator
 
 exit:
 mov $SYSEXIT, %eax
-mov %esi, %ebx
+mov $0, %ebx
 int $0x80
