7bccca41b32ed5817d025ab1bc6f1fbf429c3ad9
Alex <alex.pilkevych@gmail.com>
2018-04-30 20:07:26 +0300

    Updated makefile and README

--- lab5/README.md
+++ lab5/README.md
@@ -6,10 +6,5 @@ Clean, build and run: `make all`
 Debug: `make debug`
 
 ## Example
-1. Start debugging `make debug`
-2. Press `<Enter>`
-3. Set breakpoint at exit e.g. `b 53`
-4. Run `r`
-5. Check number in memory `i fl`
-
-`=>R7: Valid   0x400893ab85fae1480000 +590.6800524902355392`
+1. Put A, B and N values in main.c
+2. Run `make all`
--- lab5/main.c
+++ lab5/main.c
@@ -3,9 +3,9 @@ float calculate_integral(float A, float B, float N);
 
 int main()
 {
-    float A = 1;
-    float B = 90;
-    float N = 999999;
+    float A = 8;  // starting point
+    float B = 20;  // ending point
+    float N = 999999;  // number of rectangles (bigger = more precise)
     float result = calculate_integral(A, B, N);
     printf("Integral is: %f\n", result);
     return 0;
